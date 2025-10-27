a29557f991e3f9e7a4dfc3ac3d449865ac1a389d
aty2711 <angty2711@hotmail.com>
2025-10-27 17:19:48 +0800

    flask tut chap 3

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username': 'Yee'}
+    houses = [
+        {'location': 'Ang Mo Kio', 'price': 500000 , "floor_area" : 100},
+        {'location': 'Bishan', 'price': 600000, "floor_area" : 120},
+        {'location': 'Clementi', 'price': 550000, "floor_area" : 140}
+    ]
+
+    return render_template('index.html', title='Home', user=user, houses=houses)
